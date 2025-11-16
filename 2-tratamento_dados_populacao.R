ff0c8170278a29d041fbf9d0de71483539f2f177
Joel Machado Junior <107507649+JoelMJunior@users.noreply.github.com>
2025-11-16 05:41:59 -0300

    Update 2-tratamento_dados_populacao.R
    Adiciona dados relativos a população estimada de 2024 e 2025.

--- 2-tratamento_dados_populacao.R
+++ 2-tratamento_dados_populacao.R
@@ -1,10 +1,12 @@
 if(!require('dplyr'))install.packages("dplyr");library(dplyr)
+if(!require('readxl'))install.packages("readxl");library(readxl)
 
 # De 2012 a 2023 foi baixado desse link: https://basedosdados.org/dataset/d30222ad-7a5c-4778-a1ec-f0785371d1ca?raw_data_source=522f2745-3e1f-43b8-a64e-5d6c9162979a
 
 dados_populacao_antigo <- read.csv("dados_input/populacao-minas-ibge.csv")
 dados_populacao_novo <- read.csv("dados_input/ibge_populacao_municipio.csv")
-
+POP2024 <- read_excel("POP2024.xlsx", sheet = "Página1")
+POP2025 <- read_excel("POP2025.xlsx", sheet = "Página2")
 
 dados_populacao_novo <- dados_populacao_novo |>
   filter(sigla_uf == "MG") |>
@@ -19,11 +21,19 @@ dados_populacao_antigo$ano <- 2022
 dados_populacao_2023 <- dados_populacao_antigo |>
   mutate(ano = 2023)
 
-dados_populacao_2024 <- dados_populacao_antigo |>
-  mutate(ano = 2024)
+dados_populacao_2024 <- POP2024 |>
+  mutate(ano = 2024,
+         localidade = paste0("31", `COD. MUNIC`)) |>
+  mutate(localidade = as.numeric(substr(localidade, start = 1, stop = 6)),
+         populacao = `POPULAÇÃO ESTIMADA`) |>
+  select(localidade, ano, populacao)
 
-dados_populacao_2025 <- dados_populacao_antigo |>
-  mutate(ano = 2025)
+dados_populacao_2025 <- POP2025 |>
+  mutate(ano = 2025,
+         localidade = paste0("31", `COD. MUNIC`)) |>
+  mutate(localidade = as.numeric(substr(localidade, start = 1, stop = 6)),
+         populacao = `POPULAÇÃO ESTIMADA`) |>
+  select(localidade, ano, populacao)
 
 dados_populacao_antigo <- bind_rows(dados_populacao_antigo, dados_populacao_2023, dados_populacao_2024, dados_populacao_2025)
 
